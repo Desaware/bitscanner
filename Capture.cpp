ff151c5973664345adef631b389a2e8c7dc78c90
Dan Appleman <dan@desawarepublishing.com>
2025-03-12 21:28:40 -0400

    Remove extra license text.

--- Capture.cpp
+++ Capture.cpp
@@ -67,24 +67,6 @@ uint16_t Capture::getPeakVoltage()
     return result;
 }
 
-/*
-    Bitscanner - a LittleBits compatible combination oscilloscope, voltmeter and frequency counter
-    Copyright (C) 2025 by Dan Appleman
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/    
-
 uint16_t Capture::getFrequency()
 {
     return currentFrequency;
